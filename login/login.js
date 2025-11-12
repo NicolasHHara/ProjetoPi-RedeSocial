2394fd90a6c6abbfbf8466ead0a3563d837f2893
Nicolas <nico.hhara@gmail.com>
2025-11-11 21:06:56 -0300

    11.11

--- login/login.js
+++ login/login.js
@@ -1,29 +1,29 @@
 const btnSignIn = document.getElementById("btnSignIn");
-    const btnRegister = document.getElementById("btnRegister");
-    const formSignIn = document.getElementById("formSignIn");
-    const formRegister = document.getElementById("formRegister");
-    const switchText = document.getElementById("switchText");
+const btnRegister = document.getElementById("btnRegister");
+const formSignIn = document.getElementById("formSignIn");
+const formRegister = document.getElementById("formRegister");
+const switchText = document.getElementById("switchText");
 
-    btnSignIn.addEventListener("click", () => {
-      btnSignIn.classList.add("active");
-      btnRegister.classList.remove("active");
-      formSignIn.style.display = "flex";
-      formRegister.style.display = "none";
-      switchText.textContent = "Don't have an account? Register";
-    });
+btnSignIn.addEventListener("click", () => {
+  btnSignIn.classList.add("active");
+  btnRegister.classList.remove("active");
+  formSignIn.style.display = "flex";
+  formRegister.style.display = "none";
+  switchText.textContent = "Don't have an account? Register";
+});
 
-    btnRegister.addEventListener("click", () => {
-      btnRegister.classList.add("active");
-      btnSignIn.classList.remove("active");
-      formRegister.style.display = "flex";
-      formSignIn.style.display = "none";
-      switchText.textContent = "Already have an account? Sign in";
-    });
+btnRegister.addEventListener("click", () => {
+  btnRegister.classList.add("active");
+  btnSignIn.classList.remove("active");
+  formRegister.style.display = "flex";
+  formSignIn.style.display = "none";
+  switchText.textContent = "Already have an account? Sign in";
+});
 
-    switchText.addEventListener("click", () => {
-      if (formSignIn.style.display === "none") {
-        btnSignIn.click();
-      } else {
-        btnRegister.click();
-      }
-    });
+switchText.addEventListener("click", () => {
+  if (formSignIn.style.display === "none") {
+    btnSignIn.click();
+  } else {
+    btnRegister.click();
+  }
+});
