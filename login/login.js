dfffd791ad8a6b086cf9feea70f9e57623746d83
Nicolas <nico.hhara@gmail.com>
2025-11-30 17:54:29 -0300

    page login esboco

--- login/login.js
+++ login/login.js
@@ -1,29 +1,11 @@
-const btnSignIn = document.getElementById("btnSignIn");
-const btnRegister = document.getElementById("btnRegister");
-const formSignIn = document.getElementById("formSignIn");
-const formRegister = document.getElementById("formRegister");
-const switchText = document.getElementById("switchText");
+const container = document.getElementById('container');
+const registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
 
-btnSignIn.addEventListener("click", () => {
-  btnSignIn.classList.add("active");
-  btnRegister.classList.remove("active");
-  formSignIn.style.display = "flex";
-  formRegister.style.display = "none";
-  switchText.textContent = "Don't have an account? Register";
+registerBtn.addEventListener('click', () => {
+    container.classList.add("active");
 });
 
-btnRegister.addEventListener("click", () => {
-  btnRegister.classList.add("active");
-  btnSignIn.classList.remove("active");
-  formRegister.style.display = "flex";
-  formSignIn.style.display = "none";
-  switchText.textContent = "Already have an account? Sign in";
-});
-
-switchText.addEventListener("click", () => {
-  if (formSignIn.style.display === "none") {
-    btnSignIn.click();
-  } else {
-    btnRegister.click();
-  }
+loginBtn.addEventListener('click', () => {
+    container.classList.remove("active");
 });
