5538ce0a221c56d69655280e30844d35d337f138
Nicolas <nico.hhara@gmail.com>
2026-01-17 20:06:17 -0300

    17/01

---
+++ perfil-ong/perfil-ong.php
@@ -0,0 +1,56 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="perfil-ong.css">
+</head>
+<body>
+    <header>
+        <div class="div1">
+            <img src="<?php //COLOCAR A IMAGEM DA ONG?>" alt="">
+
+            <h1>
+                ONGline
+                <?php
+                //COLOCAR O NOME DA ONG, PUXANDO DO BANCO DEDAOS
+                ?>
+            </h1>
+            <?php
+                //FAZER UM VERIFICADO PARA ONGS, COLOCAR TIPO UM CORRETO DO LADO, SEMELHANTE AO INSTAGRAM
+            ?>
+        </div>
+        
+        <div class="div2">
+            <button>
+                <h2>Seguir</h2>
+            </button>
+
+            <button>
+                <h2>Doar</h2>
+            </button>
+
+            <button>
+                <h2>Voluntariar</h2>
+            </button>
+        </div>
+    </header>
+
+    <div>
+        <article>
+            <section>
+                <h2>Nossa Missão</h2>
+                <h3>
+                    <?php //EXPLICAR A MISSAO E SEUS IDEAIS DA ONG?>
+                </h3>
+            </section>
+
+            <section></section>
+        </article>
+    </div>
+</body>
+</html>
